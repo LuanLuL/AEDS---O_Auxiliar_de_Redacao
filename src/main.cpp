00c8456ce06f2d6aca7e0ea4770f142c4c1edf6c
jAzz-hub <joaogu.2001@gmail.com>
2023-06-17 15:36:47 +0000

    Testando função FirstOcurrencyWord

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+
+/*
+
+
+    Vamos pegar o vetor de parágrafos de cada palavras
+
+
+*/
+
 int main(){
     Hash h;
     ifstream arq;
@@ -12,9 +21,7 @@ int main(){
     h.AlphaOrder();
     //A=-127,-128,-126,125//E=-118 a -120//I=-115//O=-107 a -109//U=-100 a -102
     string d="Único";
-    // for(int i=0;i<int(d.length());i++){
-    //     cout<<int(d[i])<<endl;
-    // }
+    h.FirstOcurrencyWord("ezequiel");
     //cout<<int(d[0])<<int(d[1])<<endl;
     //h.AlphaOrder();
     //h.stopwords(palavra);
