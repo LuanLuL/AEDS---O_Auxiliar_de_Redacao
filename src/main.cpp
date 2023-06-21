c8acdc70a9fc8d8134f6994a64401df91146988f
Guiliard <camposrafa806@gmail.com>
2023-06-21 15:29:09 -0300

    Atualização

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ int main(){
     ifstream arq;
     string palavra = "no coco ajcu da roça teta";
     h.learquivo(arq);
+    h.AlphaOrder();
     //h.AlphaOrder();
     //A=-127,-128,-126,125//E=-118 a -120//I=-115//O=-107 a -109//U=-100 a -102
 
